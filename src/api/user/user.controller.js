9c110af5c139aef624effb45cc793f936c2f3ec3
ocaio275 <ocaio275@gmail.com>
2024-03-22 21:34:18 -0300

    feat: changing functions

--- src/api/user/user.controller.js
+++ src/api/user/user.controller.js
@@ -1,15 +1,68 @@
+const UserService = require("./user.service");
+const { isEmpty } = require("lodash");
+const ERROR_MESSAGE  = require("../../utils/error-message")
+
 class UserController {
+  constructor() {
+    this.userService = new UserService();
+  }
   async findAllUsers(req, res) {
-    return res.status(200).json({ message: "users found" });
+    try {
+      const users = await this.userService.findAllUsers();
+      return res.status(200).json({ payload: users });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   }
   async createUser(req, res) {
-    return res.status(200).json({ message: "user created" });
+    const { email, password, confirmPassword } = req.body;
+
+    try {
+      if (isEmpty(email)) throw new Error(ERROR_MESSAGE.USER.EMAIL_EMPTY);
+      if (isEmpty(password)) throw new Error(ERROR_MESSAGE.USER.PASSWORD_EMPTY);
+      if (isEmpty(confirmPassword))
+        throw new Error(ERROR_MESSAGE.USER.CONFIRM_PASSWORD_EMPTY);
+
+      await this.userService.createUser({ email, password, confirmPassword });
+
+      return res.status(201).json({ message: "user created" });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
   }
-  async updateUser(req, res) {
-    return res.status(200).json({ message: "user updated" });
+  async updatePassword(req, res) {
+    try {
+      const { id } = req.params;
+      const { oldPassword, newPassword, confirmPassword } = req.body;
+
+      if (isEmpty(id)) throw new Error(ERROR_MESSAGE.USER.ID_EMPTY);
+      if (isEmpty(oldPassword)) throw new Error(ERROR_MESSAGE.USER.PASSWORD_EMPTY)
+      if (isEmpty(newPassword)) throw new Error(ERROR_MESSAGE.USER.NEW_PASSWORD_EMPTY)
+      if (isEmpty(confirmPassword)) throw new Error(ERROR_MESSAGE.USER.CONFIRM_PASSWORD_EMPTY)
+
+      await this.userService.updatePassword({ id, oldPassword, newPassword, confirmPassword });
+
+      return res.status(200).json({ message: "Updated password" });
+
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
   }
-  async deleteUser(req, res) {
-    return res.status(200).json({ message: "user deleted" });
+  async deleteUser(req, res) {    
+    try {
+      const { id } = req.params
+      const { password, confirmPassword } = req.body
+  
+      if (isEmpty(id)) throw new Error(ERROR_MESSAGE.USER.ID_EMPTY);
+      if (isEmpty(password)) throw new Error(ERROR_MESSAGE.USER.PASSWORD_EMPTY)
+      if (isEmpty(confirmPassword)) throw new Error(ERROR_MESSAGE.USER.CONFIRM_PASSWORD_EMPTY)
+  
+      await this.userService.deleteUser({ id, password, confirmPassword });
+      return res.status(200).json({ message: "user deleted" });
+      
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
 }
 
