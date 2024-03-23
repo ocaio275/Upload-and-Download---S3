e718e7b01b2e158ee47eb7928ed64db0d0ab2c38
ocaio275 <ocaio275@gmail.com>
2024-03-22 21:41:51 -0300

    add: userRouter and AuthRouter

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,8 +1,12 @@
-const UserRouter = require('../../api/user/user.route')
-const { API_VERSION } = process.env
+const router = require("express").Router();
+const UserRouter = require("../../api/user/user.routes");
+const AuthRouter = require("../../api/auth/auth.routes");
+const { API_VERSION } = process.env;
 
-const Router = (app) => {
-    app.use(API_VERSION, UserRouter);
-}
+const Router = async (app) => {
+  new UserRouter(router);
+  new AuthRouter(router);
+  app.use(API_VERSION, router);
+};
 
-module.exports = Router
+module.exports = Router;
