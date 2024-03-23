b44387191c5ff01b1dce66e19bcbdef3982571d6
ocaio275 <ocaio275@gmail.com>
2024-03-22 21:37:44 -0300

    create error message

---
+++ src/utils/error-message.js
@@ -0,0 +1,16 @@
+const ERROR_MESSAGE = {
+  USER: {
+    EMAIL_EMPTY: "Email must not be empty",
+    PASSWORD_EMPTY: "Password must not be empty",
+    CONFIRM_PASSWORD_EMPTY: "Confirm password must not be empty",
+    EMAIL_ALREADY_IN_USE: "Email already in use",
+    PASSWORD_DOES_NOT_MATCH: "Passwords do not match",
+    OLD_PASSWORD_DOES_NOT_MATCH: "Old passwords do not match",
+    USER_NOT_FOUND: "User not found",
+    ID_EMPTY: "Id is required",
+    NEW_PASSWORD_EMPTY: "New password is required",
+    PASSWORD_INCORRECT: "Password is incorrect",
+  },
+};
+
+module.exports = ERROR_MESSAGE;
