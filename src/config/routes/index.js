c4b886467b56e668da8fd863c5a71a1f1689b6a6
ocaio275 <ocaio275@gmail.com>
2024-03-26 14:21:56 -0300

    changing router AuthRouter -> DocumentRouter

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const UserRouter = require("../../api/user/user.routes");
-const AuthRouter = require("../../api/auth/auth.routes");
+const DocumentRouter = require("../../api/document/document.routes")
+
 const { API_VERSION } = process.env;
 
 const Router = async (app) => {
   new UserRouter(router);
-  new AuthRouter(router);
+  new DocumentRouter(router);
   app.use(API_VERSION, router);
 };
 
